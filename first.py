6f8702a998decfcc24dcc9f94aed6c39310d6cfd
Yaroslav-Savchenko <y.savchenko10@gmail.com>
2025-10-31 16:06:41 +0200

    some_changes

--- first.py
+++ first.py
@@ -1,7 +1,4 @@
-side_a = 10
-side_b = 5
-hupathenuse = (side_a**2+side_b**2)**0.5
-print (hupathenuse)
-
-
+words = ["banana", "apple", "cherry"]
+words.sort(key=len)
+print(words)  # Виведе ['apple', 'banana', 'cherry']
 
